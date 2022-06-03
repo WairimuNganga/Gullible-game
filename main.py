82c7109eea14e7149edc4bf315bcd4d5bce56f3b
WairimuNganga <wairimuznganga@gmail.com>
2022-06-03 08:15:03 +0300

    single apostrophe

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #How to keep a gullible person busy
 while True:
-    print("Do you know want to know how to keep a gullible person busy? Y/N ")
-    response = input("> ")
+    print('Do you know want to know how to keep a gullible person busy? Y/N ')
+    response = input('> ')
     if response.lower()=='n' or response.lower()=='no':
         break
     if response.lower()=='y' or response.lower()=='yes':
